1c9526f2294a1c70ed6e83ab5874413cd1e2fbcc
Julien Aubert <j.aubert@actongroup.com>
2023-09-28 18:52:34 +0200

    smtp

--- src/main/java/fr/mi/dso/bouchons/ApiDemoController.java
+++ src/main/java/fr/mi/dso/bouchons/ApiDemoController.java
@@ -2,8 +2,9 @@ package fr.mi.dso.bouchons;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,26 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiDemoController {
 
-    @Autowired
-    private JavaMailSender sender;
+	@Autowired
+	private JavaMailSender sender;
 
-    @GetMapping("/mails")
-    public String bouchonMail(@RequestHeader Map<String, String> headers) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("mon-app@interieur-numerique.com");
-        message.setTo("p.leclainche@actongroup.com");
-        String subject = "Mail de test";
-        String content = " Header from Mellon :\n";
+	@GetMapping("/mails")
+	public String bouchonMail(@RequestHeader Map<String, String> headers) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("mon-app@interieur-numerique.com");
+		message.setTo("p.leclainche@actongroup.com");
+		String subject = "Mail de test";
+		String content = " Header from Mellon :\n";
 
-        content += "Mellon-NameID : " + headers.get("Mellon-NameID") + "\n";
-        content += "Mellon-Groups : " + headers.get("Mellon-Groups") + "\n";
-        content += "Mellon-Role : " + headers.get("Mellon-Role") + "\n";
+		content += "Mellon-NameID : " + headers.get("Mellon-NameID") + "\n";
+		content += "Mellon-Groups : " + headers.get("Mellon-Groups") + "\n";
+		content += "Mellon-Role : " + headers.get("Mellon-Role") + "\n";
 
-        message.setSubject(subject);
-        message.setText(content);
-        sender.send(message);
+		message.setSubject(subject);
+		message.setText(content);
+		sender.send(message);
 
-        return "ok";
-    }
+		return "ok";
+	}
 
 }
